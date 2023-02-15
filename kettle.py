375048e6f47131cd71aa1201fc4e60e3e1fdc726
Anna Yashina <a.yashina.corp@gmail.com>
2023-02-16 04:56:06 +0600

    add sqlite3

--- kettle.py
+++ kettle.py
@@ -1,7 +1,17 @@
 import logging
+import sqlite3
 import time
 
 import settings
+
+
+sqlite_conn: sqlite3.Connection
+sqlite_cursor: sqlite3.Cursor
+
+
+def sqlite_message(message: str):
+    sqlite_cursor.execute('INSERT INTO logs(message) VALUES (?);', [message])
+    sqlite_conn.commit()
 
 
 class Kettle:
@@ -18,6 +28,8 @@ class Kettle:
               f'Температура воды:\n{self.t_water:.0f}°C')
         logging.info('Kettle is turned on')
         logging.info(f't water: {self.t_water:.0f}°C')
+        sqlite_message('Kettle is turned on')
+        sqlite_message(f't water: {self.t_water:.0f}°C')
         t_step = (self.t_water_stop - self.t_water) / self.boil_time
         is_kettle_boiled = False
         for x in range(self.boil_time):
@@ -25,18 +37,22 @@ class Kettle:
             self.t_water += t_step
             print(f'{self.t_water:.0f}°C')
             logging.info(f't water: {self.t_water:.0f}°C')
+            sqlite_message(f't water: {self.t_water:.0f}°C')
             if not is_kettle_boiled and round(self.t_water) >= 100:
                 is_kettle_boiled = True
                 print('Чайник вскипел.')
                 logging.info('Kettle is boiled')
+                sqlite_message('Kettle is boiled')
         self.stop()
 
     def stop(self):
         """Остановка и выключение чайника."""
         print('\nЧайник остановлен.')
         logging.info('Kettle is stopped')
+        sqlite_message('Kettle is stopped')
         print('\nЧайник выключен.')
         logging.info('Kettle is turned off')
+        sqlite_message('Kettle is turned off')
 
     def pour_water(self, water_level: float):
         """Налитие воды в чайник."""
@@ -50,8 +66,17 @@ if __name__ == '__main__':
         datefmt='%d.%m.%y %H:%M:%S',
         level=logging.DEBUG
     )
-    kettle = Kettle()
+    sqlite_conn = sqlite3.connect('orders.db')
+    sqlite_cursor = sqlite_conn.cursor()
+    sqlite_cursor.execute(
+        'CREATE TABLE IF NOT EXISTS logs ('
+        'id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
+        'message VARCHAR(64),'
+        'datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL );'
+    )
+    sqlite_conn.commit()
 
+    kettle = Kettle()
     print(f'Программа Чайник.\n\nСколько наливаете воды? (0 - '
           f'{kettle.max_water_level}): ' , end='')
     # обработка ввода пользователя
